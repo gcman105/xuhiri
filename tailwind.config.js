e987b8993f76d3591566f4fe8d1038779a18cc9b
Gary Cheeseman <gary@five57.com>
2021-12-05 19:46:29 +0100

    tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 // const defaultTheme = require('tailwindcss/defaultTheme')
 import { defaultTheme } from 'tailwindcss/defaultTheme'
-import { colors } from 'tailwindcss/colors'
+// import { colors } from 'tailwindcss/colors'
 
 module.exports = {
     purge: [
@@ -18,10 +18,9 @@ module.exports = {
     },
     variants: {},
     plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/line-clamp'),
-        require('@tailwindcss/aspect-ratio')
+        // require('@tailwindcss/typography'),
+        // require('@tailwindcss/forms'),
+        // require('@tailwindcss/line-clamp'),
+        // require('@tailwindcss/aspect-ratio')
     ],
 }
